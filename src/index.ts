21009774205b66da7a26cd3be3ac3f73a788ca1c
Martix <66840932+MartixInTheMatrix@users.noreply.github.com>
2022-04-18 21:30:30 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import discord from "discord.js";
+import { Client } from "./structure/Client"
+
+/*
+export const client = new Client('token')
+client.init('./build/commands', './build/events')
+*/
